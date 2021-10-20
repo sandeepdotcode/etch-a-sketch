1593f8b2dba2de0486bf9102b2fca4569be7429b
Sandeep K K <sandeepdotcode@gmail.com>
2021-10-20 13:19:19 +0530

    Add rainbow color

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
+let currentMode = 0;                // 0 - normal, 1 - rainbow
+let prevMode = 0;
+
 const container = document.querySelector('.container');
 const clearBtn = document.querySelector('.clear-button');
 const colorInput = document.querySelector('.color-picker');
 const slider = document.querySelector('.slider');
+const normBtn = document.querySelector('.normal-button');
+const randBtn = document.querySelector('.random-button');
+
+// color value grabbed from color input
+// once when page is loaded and dynamically on 'change' event
+let color = colorInput.value;
 
 // function to create the grid
 function createGrid(size) {
@@ -20,8 +29,44 @@ function createGrid(size) {
     }
 }
 
+function normMode() {
+    if (currentMode === 1) {
+        prevMode = currentMode;
+        currentMode = 0;
+        normBtn.classList.add('active');
+        randBtn.classList.remove('active');
+    }
+}
+function getRandomVal() {
+    return Math.floor(Math.random() * (256 - 0) + 0);
+}
+
+function getRandomColor() {
+    return 'rgb(' + getRandomVal() + ', ' + getRandomVal() + ', ' + getRandomVal() + ')';
+}
+
+function randomMode() {
+    randBtn.classList.toggle('active');
+    if (currentMode === 0) {
+        prevMode = currentMode;
+        currentMode = 1;
+        normBtn.classList.remove('active');
+    }
+    else if (currentMode === 1) {             // toggle random color off
+        prevMode = 1;
+        currentMode = 0;
+        normBtn.classList.add('active');
+    }
+}
+
 function changeColor(e) {
-    e.target.style.backgroundColor = `${color}`;
+    if (currentMode === 0) {
+        e.target.style.backgroundColor = `${color}`;
+    }
+    else if (currentMode == 1) {
+        e.target.style.backgroundColor = `${getRandomColor()}`;
+    }
+    
 }
 
 function clearGrid() {
@@ -36,30 +81,27 @@ function reloadGrid(size) {
     createGrid(size);
 }
 
-// display slider value above it
+// display slider value above the slider
 function rangeSlide(value) {
     document.getElementById('slider-value').textContent = `${value} x ${value}`;
 }
 
-
-
 // Initializes a 16x16 grid when page is loaded
 createGrid(16);
-
-// color value grabbed from color input
-// once when page is loaded and dynamically on 'change' event
-let color = colorInput.value;
 
 colorInput.addEventListener('change', () => {
     color = colorInput.value;
 });
 
+
+normBtn.addEventListener('click', normMode);
+randBtn.addEventListener('click', randomMode);
 clearBtn.addEventListener('click', clearGrid);
 
-// event handlers to display slider value changing
-slider.addEventListener('change', e => rangeSlide(e.target.value));
+// event handler to display slider value changing
 slider.addEventListener('mousemove', e => rangeSlide(e.target.value));
 
-// event handlers to rebuild the grid with new slider value
+// event handler to rebuild the grid with new slider value
 slider.addEventListener('input', (e) => reloadGrid(e.target.valueAsNumber));
 slider.addEventListener('change', (e) => reloadGrid(e.target.valueAsNumber));
+
